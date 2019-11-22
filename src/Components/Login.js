f588aab8017db3c43e7aa54666353313de02bdfa
purlwon <purlwon@naver.com>
2019-11-22 16:58:54 +0900

    Google, Facebook Login setting

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-//import { GoogleLogin } from 'react-google-login';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
 class Login extends React.Component {
     //백엔드 정보 저장 state = 로그인사용자id, name, providers(google, facebook, naver)
@@ -13,6 +14,14 @@ class Login extends React.Component {
     }
     //Google Login
     responseGoogle = (res) => {
+        this.setState({
+            id: res.goodleId, 
+            name: res.profileObj.name, 
+            provider: 'google'
+        })
+    }
+    //Facebook Login
+    responseFacebook = (res) => {
         console.log(res);
     }
     //Login Fail
@@ -24,7 +33,18 @@ class Login extends React.Component {
         return (
             <div>
                 <h1>Login</h1>
-                
+                <FacebookLogin 
+                    appId="535326200382462"
+                    fields="name,email"
+                    callback={this.responseFacebook}    
+                />
+                <br />
+                <GoogleLogin 
+                    clientId="992297699065-e1ge0d7k265rp8hoidikr7hucdh5mtnl.apps.googleusercontent.com"
+                    buttonText="LOGIN WITH GOOGLE"
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseGoogle}
+                />
             </div>
         );
     }
